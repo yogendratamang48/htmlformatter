6bd7affed0c84fe85bd1ae9d52bdaf5eb034b82f
Yogendra Tamang <48yogen@gmail.com>
2018-04-29 08:30:49 +0545

    minor fix

--- formatter.py
+++ formatter.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup, ModifyHTML
+from bs4 import BeautifulSoup
 import html2text
 import requests
 import urllib.request
@@ -6,7 +6,7 @@ import re
 from html.parser import HTMLParser
 from os import linesep
 import pudb
-pudb.set_trace()
+#pudb.set_trace()
 
 URL = 'https://www.sec.gov/Archives/edgar/data/1405073/0001193125-17-030376.txt'
 
@@ -21,24 +21,20 @@ def cleanhtml(raw_html):
     text = soup.get_text()
     return text.encode('ascii', errors='ignore')
 # modify HTML.
-def test_html():
-    html = open("test.html").read()
-    html = ModifyHTML(html, "html5lib") #BeautifulSoup.
-    html.shift_links() #rewrite links as in requirements, above.
-    html.remove_images() #remove images as in requirements.
-    html = html.raw() #back to string.
-     
+def test_html(html):
     # convert HTML to text.
-    h2t = HTMLToText()
-    plain = h2t.text(html, is_raw=True)
-    print(plain)
+    h = html2text.html2text(html)
+    y = h.encode('ascii', errors='ignore')
+    return y
+
 def main_task():
-    response = urllib.request.urlopen(URL)
-    content = response.read()
-    text = cleanhtml(content.decode('utf-8'))
-    text = HtmlTool.to_nice_text(text)
+    #response = urllib.request.urlopen(URL)
+    #content = response.read()
+    content = open('test.html').read()
+    #text = test_html(content.decode('utf-8'))
+    text = test_html(content)
     with open('extracted.txt', 'w') as text_file:
-        text_file.write(text)
+        text_file.write(text.decode('utf-8'))
 
 if __name__=='__main__':
-    test_html()
+    main_task()
